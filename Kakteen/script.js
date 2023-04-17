e638b4abdfe643fcc1f89ace4948ffc6c19acc2e
Colin Z <keinaniemand@gmail.com>
2023-04-17 13:35:27 +0200

    Erste Website Homepage Version von Colin

---
+++ Kakteen/script.js
@@ -0,0 +1,37 @@
+function cycle(){
+	const active = document.querySelector(".act");
+	const index = Number(active.dataset.index);
+	var nindex = Number(index) + 1;
+	
+	if (nindex>getCards()){
+		nindex = 1;
+	}
+	
+	active.classList.add("out");
+	active.classList.remove("in")
+	
+	const snindex = nindex.toString();
+	active.classList.remove("act");
+	const snindexString = `[data-index='${snindex}']`;
+	const next = document.querySelector(snindexString);
+	next.classList.add("in");
+	next.classList.remove("out");
+	next.classList.add("act");
+}
+
+function getCards(){
+	var cardnum = 0;
+	Array.from(document.querySelectorAll('.card')).forEach((element,index) =>
+	{
+		 if (Number(element.dataset.index)>cardnum){
+			 cardnum = Number(element.dataset.index);
+		 }
+	});
+	
+	return cardnum;
+}
+
+Array.from(document.querySelectorAll('.card')).forEach((element,index) =>
+{
+	 element.addEventListener('click', cycle);
+});
