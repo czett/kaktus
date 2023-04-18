b0948a9761d68196971df721a40391a88372c51f
Viegas Da Silva <fabio.viegas-da-silva@adesso.de>
2023-04-18 10:41:09 +0200

    gsgd

--- Kakteen/flask_backend/app.py
+++ Kakteen/flask_backend/app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def start():
 	return render_template("index.html")
 
+@app.route("/shop")
+def shop():
+	return render_template("Shop.html")
+
+@app.route("/entdecken")
+def entdecken():
+	return render_template("Entdecken.html")
+
+@app.route("/support")
+def support():
+	return render_template("Support.html")
+
+@app.route("/profil")
+def profil():
+	return render_template("Profil.html")
+
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
