d91a7ef552ab51ce337d09ee6f96d101b2fe9ccf
Ziegler <colin.ziegler@adesso.de>
2023-04-18 09:40:25 +0200

    flask

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import os
 
 app = Flask(__name__)
 
-template_dir = os.path.abspath('../../frontend/src')
+template_dir = os.path.abspath('Kakteen/flask_backend')
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/")
 def start():
-	return "Hola Ninos"
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
