67f94c6c33e522a27cdb07e97b375c4273069917
Dirve <noah.dirven@adesso.de>
2023-04-22 14:06:50 +0200

    animation fix

--- Kakteen/flask_backend/static/animation_js.js
+++ Kakteen/flask_backend/static/animation_js.js
@@ -3,6 +3,8 @@
 var balli = document.getElementById('animateBall');
 var boxi = document.getElementById('anim-uebergang');
 
+// Shop
+
 document.addEventListener('DOMContentLoaded', function () {
 	var move = [
 
@@ -15,74 +17,34 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 
 		{
-            transform: 'translate(45vw, 95vh)',
-            width: '5vw',
-            height: '5vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(90vw, 45vh)',
-            width: '10vw',
-            height: '10vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '5vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0vw, 85vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '25vw',
-            height: '25vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0, 0)',
+            transform: 'translate(0vw, 0vh)',
             width: '100vw',
             height: '100vh',
             borderRadius: '0vw',
 			visibility: 'visible',
-        },
-
-        
+        },    
 ];
 	
     document.querySelector('#shopPress').addEventListener('click', function() {
             balli.animate(move, {
-				duration: 1000,
+				duration: 220,
 				iterations: 1,
 				fill: 'forwards'
 			});
             setTimeout(function(){
                 window.location.href = "/shop";
-            }, 1050);
+            }, 230);
         } 
 );
-
 });
 
+// Home
+
 var balli = document.getElementById('animateBall');
+var boxi = document.getElementById('anim-uebergang');
 
 document.addEventListener('DOMContentLoaded', function () {
-	var move2 = [
+	var moveA = [
 
         {
             transform: 'translate(58vw, 2vh)',
@@ -93,75 +55,31 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 
 		{
-            transform: 'translate(45vw, 95vh)',
-            width: '5vw',
-            height: '5vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(90vw, 45vh)',
-            width: '10vw',
-            height: '10vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '5vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0vw, 85vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '25vw',
-            height: '25vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0, 0)',
+            transform: 'translate(0vw, 0vh)',
             width: '100vw',
             height: '100vh',
             borderRadius: '0vw',
 			visibility: 'visible',
-        },
-
-        
+        },    
 ];
-
 	
     document.querySelector('#homePress').addEventListener('click', function() {
-            balli.animate(move2, {
-				duration: 1000,
+            balli.animate(moveA, {
+				duration: 220,
 				iterations: 1,
 				fill: 'forwards'
 			});
             setTimeout(function(){
                 window.location.href = "/";
-            }, 1050);
+            }, 230);
         } 
 );
-
 });
 
-var balli = document.getElementById('animateBall');
+// Entdechen
 
 document.addEventListener('DOMContentLoaded', function () {
-	var move3 = [
+	var moveB = [
 
         {
             transform: 'translate(72vw, 2vh)',
@@ -172,75 +90,31 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 
 		{
-            transform: 'translate(45vw, 95vh)',
-            width: '5vw',
-            height: '5vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(90vw, 45vh)',
-            width: '10vw',
-            height: '10vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '5vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0vw, 85vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '25vw',
-            height: '25vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0, 0)',
+            transform: 'translate(0vw, 0vh)',
             width: '100vw',
             height: '100vh',
             borderRadius: '0vw',
 			visibility: 'visible',
-        },
-
-        
+        },    
 ];
-
 	
     document.querySelector('#discoverPress').addEventListener('click', function() {
-            balli.animate(move3, {
-				duration: 1000,
+            balli.animate(moveB, {
+				duration: 220,
 				iterations: 1,
 				fill: 'forwards'
 			});
             setTimeout(function(){
                 window.location.href = "/entdecken";
-            }, 1050);
+            }, 230);
         } 
 );
-
 });
 
-var balli = document.getElementById('animateBall');
+// Kakteen Support
 
 document.addEventListener('DOMContentLoaded', function () {
-	var move4 = [
+	var moveC = [
 
         {
             transform: 'translate(79vw, 2vh)',
@@ -251,69 +125,25 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 
 		{
-            transform: 'translate(45vw, 95vh)',
-            width: '5vw',
-            height: '5vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(90vw, 45vh)',
-            width: '10vw',
-            height: '10vh',
-            borderRadius: '100vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '5vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0vw, 85vh)',
-            width: '15vw',
-            height: '15vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(45vw, 0vh)',
-            width: '25vw',
-            height: '25vh',
-            borderRadius: '50vw',
-			visibility: 'visible',
-        },
-
-        {
-            transform: 'translate(0, 0)',
+            transform: 'translate(0vw, 0vh)',
             width: '100vw',
             height: '100vh',
             borderRadius: '0vw',
 			visibility: 'visible',
-        },
-
-        
+        },    
 ];
-
 	
     document.querySelector('#supportPress').addEventListener('click', function() {
-            balli.animate(move4, {
-				duration: 1000,
+            balli.animate(moveC, {
+				duration: 220,
 				iterations: 1,
 				fill: 'forwards'
 			});
             setTimeout(function(){
                 window.location.href = "/support";
-            }, 1050);
+            }, 230);
         } 
 );
-
 });
 
 // quiz animation
@@ -398,5 +228,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 //==============/Animation with side change [end]\==========\\
-
-
